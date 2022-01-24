c96dde7d7ca267a1c6ba8eb7d4e627c3d6f22427
JackSolano <Jackfrediandressolano@gmail.com>
2022-01-24 12:50:39 -0500

    cargar archivos

---
+++ Proyecto2/CargarFoto.js
@@ -0,0 +1,16 @@
+var table = document.getElementById("tablero").getContext("2d");
+
+var foto = {url: "JuliaMaggi.png"};
+
+foto.imagen = new Image() ;
+foto.imagen.src = foto.url;
+foto.imagen.addEventListener("load", cargarFoto) ;
+
+function cargarFoto(){
+  foto.cargaOK = true;
+  dibujar() ;
+}
+
+function dibujar(){
+  table.drawImage(foto.imagen,0,0);
+}
